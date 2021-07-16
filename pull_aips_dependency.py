15e2e588c5520330efce81b4ffafbab2a1661022
Max Irwin <max_irwin@yahoo.com>
2021-07-16 08:44:16 -0400

    split

--- pull_aips_dependency.py
+++ pull_aips_dependency.py
@@ -7,11 +7,13 @@ def pull_dependency(file_name):
   with open(file_name, "wb") as file:
     for part in map(chr,range(ord('a'),ord('z')+1)):
       part_name = "part_" + str(part)
-      response = requests.get("https://github.com/ai-powered-search/outdoors/raw/main/" + file_name + "/" + part_name)
+      url = "https://github.com/ai-powered-search/outdoors/raw/main/" + file_name + "." + part_name
+      response = requests.get(url)
       if response.status_code == 200:
         print("Successfully downloaded " + part_name)
         file.write(response.content)
       elif response.status_code == 404:
+        print('404')
         break
       else:
         raise Exception("Error: Status Code " + response.status_code + "\n" + response.text)
